a025f6f8d68e184e1ba1c83c7387e2b2ecf2eead
kateryna klimchuk <ketyclima@gmail.com>
2022-05-03 20:39:11 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body')
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick () {
+    timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startBtn.disabled = true;
+}
+function onStopBtnClick() {
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
